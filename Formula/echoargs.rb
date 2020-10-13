163671603b5931f3d31e362554f706daa3e6de20
Jeff McLean <44034094+maxjeffos@users.noreply.github.com>
2020-10-13 16:58:24 -0400

    Update echoargs.rb

--- Formula/echoargs.rb
+++ Formula/echoargs.rb
@@ -5,7 +5,7 @@ class Echoargs < Formula
 
 
   url "https://raw.githubusercontent.com/maxjeffos/echoargs/master/bundle/bundle.tar.gz"
-  sha256 "e651dce032bceed84f17289f25a4574270d567803665023a3c4d45dcb944fa25"
+  sha256 "d5d3290000760cbc2b824d031d4f2480c0cabd7698ad83a8a86a24ac2e204321"
 
   def install
     bin.install "echoargs-bin" => "echoargs-bin"
