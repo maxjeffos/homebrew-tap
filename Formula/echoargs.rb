47b99346df2c1412fef835157b64455d97913032
Jeff McLean <44034094+maxjeffos@users.noreply.github.com>
2020-10-13 13:03:07 -0400

    Create echoargs.rb

---
+++ Formula/echoargs.rb
@@ -0,0 +1,13 @@
+class EchoArgs < Formula
+  desc "Brew Tap Experiment; echo input args"
+  homepage "https://github.com/maxjeffos/homebrew-tap"
+  version "v0.0.1"
+
+  url "https://github.com/maxjeffos/echoargs/blob/master/bin/echoargs"
+  sha256 "aad9fe11bcc27e609d27ff6c224a70ae948985ba4c4da3197a3789b1bd8d65fe"
+
+  def install
+    bin.install echoargs => "echoargs"
+  end
+
+end
