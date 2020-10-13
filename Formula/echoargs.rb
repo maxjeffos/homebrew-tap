37504a4734996af6b2dd057b7c4820291ed85b82
Jeff McLean <44034094+maxjeffos@users.noreply.github.com>
2020-10-13 14:08:42 -0400

    Update echoargs.rb

--- Formula/echoargs.rb
+++ Formula/echoargs.rb
@@ -3,12 +3,19 @@ class Echoargs < Formula
   homepage "https://github.com/maxjeffos/homebrew-tap"
   version "v0.0.1"
 
-  url "https://raw.githubusercontent.com/maxjeffos/echoargs/master/bin/echoargs"
-  sha256 "aad9fe11bcc27e609d27ff6c224a70ae948985ba4c4da3197a3789b1bd8d65fe"
+  resource do
+    url "https://raw.githubusercontent.com/maxjeffos/echoargs/master/bin/echoargs"
+    sha256 "aad9fe11bcc27e609d27ff6c224a70ae948985ba4c4da3197a3789b1bd8d65fe"
+  end
+  
+  resource do
+    url "https://raw.githubusercontent.com/maxjeffos/homebrew-tap/blob/master/Formula/echo-args-test.sh"
+    sha256 "aad9fe11bcc27e609d27ff6c224a70ae948985ba4c4da3197a3789b1bd8d65fe"
+  end
 
   def install
     bin.install "echoargs" => "echoargs"
-    bin.install "echo-args-test.sh" => "echo-args-test.sh" 
+    bin.install "echo-args-test.sh" => "echo-args-test" 
   end
 
 end
